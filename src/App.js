c177cea4112b2b79c119e2d4b41be873bf69a4d1
Hritik Arora <hritikarora@Hritiks-MacBook-Pro.local>
2023-11-28 18:32:14 -0400

    some change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This is main
+          This is prod
         </a>
       </header>
     </div>
